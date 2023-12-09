e6f3231c0bb8904c7d8436c02bcc190edea63ba8
chokzass2007 <84018218+chokzass2007@users.noreply.github.com>
2023-12-09 15:30:25 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// rafce
+import React from 'react'
+import Navbars from './components/Navbars'
+import Hero from './components/Hero'
+import Abount from './components/Abount'
+import Experience from './components/Experience'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbars/>
+      <Hero/>
+      <Abount/>
+      <Experience/>
+    </div>
+  )
+}
+
+export default App
